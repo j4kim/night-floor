7a2cb8319140b093402fd401d9091b77770a4179
Joaquim Perez <jivkim@gmail.com>
2025-02-07 20:33:02 +0100

    add a light dependen resistor and prints its value

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from machine import Pin
+from machine import Pin, ADC
 from time import sleep
 
 led = Pin(25, Pin.OUT)
 
 pir = Pin(3, Pin.IN)
+
+ldr = ADC(Pin(26))
+
+print(dir(ldr))
 
 print("Running")
 
@@ -13,3 +17,4 @@ while True:
         sleep(0.1)
     else:
         sleep(0.5)
+    print(ldr.read_u16())
