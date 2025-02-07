d2149ac36e07b2faac44f9317425c39c107dba82
Joaquim Perez <jivkim@gmail.com>
2025-02-07 18:43:24 +0100

    connect PIR sensor, change blink frequency if movement is detected

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-from machine import Pin # Import the Pin class from the machine module
-from time import sleep  # Import the sleep function from the time module
+from machine import Pin
+from time import sleep
 
-# Initialize GPIO25 as an output pin, which controls the USER LED
-led = Pin(25, Pin.OUT) 
+led = Pin(25, Pin.OUT)
 
-# Turn off the LED initially
-led.value(1) # led.on() -> high level -> light off
-sleep(0.5) # Wait for 0.5 seconds
+pir = Pin(3, Pin.IN)
 
-# Turn on the LED
-led.value(0) # led.off() -> low level -> light on
-sleep(0.5) # Wait for 0.5 seconds
+print("Running")
 
-# Enter an infinite loop
 while True:
-    # Toggle the LED state (on to off or off to on)
-    led.toggle() 
-    # Print the current state of the LED
-    print(f"LED {'ON' if led.value() == 0 else 'OFF'}")
-    sleep(0.5) # Wait for 0.5 seconds before the next toggle
+    led.toggle()
+    if pir.value():
+        sleep(0.1)
+    else:
+        sleep(0.5)
