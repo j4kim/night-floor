56b8415d32aa136a41d2b18a03ca62def9897faa
Joaquim Perez <jivkim@gmail.com>
2025-02-07 16:23:50 +0100

    blink.py -> main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# main.py -- put your code here!
+from machine import Pin # Import the Pin class from the machine module
+from time import sleep  # Import the sleep function from the time module
+
+# Initialize GPIO25 as an output pin, which controls the USER LED
+led = Pin(25, Pin.OUT) 
+
+# Turn off the LED initially
+led.value(1) # led.on() -> high level -> light off
+sleep(0.5) # Wait for 0.5 seconds
+
+# Turn on the LED
+led.value(0) # led.off() -> low level -> light on
+sleep(0.5) # Wait for 0.5 seconds
+
+# Enter an infinite loop
+while True:
+    # Toggle the LED state (on to off or off to on)
+    led.toggle() 
+    # Print the current state of the LED
+    print(f"LED {'ON' if led.value() == 0 else 'OFF'}")
+    sleep(0.5) # Wait for 0.5 seconds before the next toggle
