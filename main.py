27a4d89679176a711ead57b881cd3678a5929f61
Joaquim Perez <jivkim@gmail.com>
2025-03-07 18:58:05 +0100

    new Table class, remove all logic

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
 from machine import Pin, ADC
 from time import sleep
 from tools import Neopixel
+from table import Table
 
-pir = Pin(26, Pin.IN)
 ldr = ADC(Pin(27))
 
-led = Neopixel(Pin(15), 7, 0.2)
-
-orangy = (255, 127, 20)
-
-pir_val = 0
-prev_pir_val = 0
-lit = False
+table_2 = Table(
+    name = "Table 2",
+    ldr = ldr,
+    btn = Pin(28, Pin.IN, Pin.PULL_UP),
+    switch_pir = Pin(13, Pin.IN, Pin.PULL_UP),
+    switch_led = Pin(14, Pin.IN, Pin.PULL_UP),
+    pir = Pin(26, Pin.IN),
+    led = Neopixel(Pin(15), 7, 0.2)
+)
 
 while True:
-    ldr_value = ldr.read_u16()
-    night = ldr_value > 5000
-    pir_val = pir.value()
-
-    if (pir_val != prev_pir_val):
-        print("PIR state change", pir_val, night)
-        if pir_val:
-            if night:
-                led.fade_in(orangy, 9)
-                lit = True
-                print("Stay lit for 5 seconds")
-                sleep(5)
-            else:
-                print("It is not night")
-        else:
-            if lit:
-                led.fade_out(orangy)
-                lit = False
-
-    prev_pir_val = pir_val
+    table_2.loop()
 
     sleep(0.1)
---
+++ table.py
@@ -0,0 +1,29 @@
+class Table:
+    def __init__(self, name, ldr, btn, switch_pir, switch_led, pir, led):
+        """Initialize a Table object with sensors and controls.
+        
+        Args:
+            name (str): Name identifier for the table
+            ldr (ADC): Light Dependent Resistor analog input
+            btn (Pin): Button input pin
+            switch_pir (Pin): PIR enable/disable switch
+            switch_led (Pin): LED enable/disable switch
+            pir (Pin): PIR motion sensor input
+            led (Neopixel): LED strip controller
+        """
+        self.name = name
+        self.ldr = ldr
+        self.btn = btn
+        self.switch_pir = switch_pir
+        self.switch_led = switch_led
+        self.pir = pir
+        self.led = led
+
+    def loop(self):
+        print(
+            self.ldr.read_u16(),
+            self.btn.value(),
+            self.switch_pir.value(),
+            self.switch_led.value(),
+            self.pir.value()
+        )
